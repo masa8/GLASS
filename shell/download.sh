1991032a99e3d17a5eff7685dc8e50b004d4db1b
masa8 <masa.shimomatsu@gmail.com>
2026-02-08 09:45:17 +0900

    update

--- shell/download.sh
+++ shell/download.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+
+#wget https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz
+#tar -xf dtd-r1.0.1.tar.gz
+#pip install -U "numpy<2" "imgaug==0.4.0"
+
 # ===== 設定 =====
 FOLDER_ID="1a0onTe-bOrO6bzGZPwlUNZAY-_mFE1fJ"
 OUT_DIR="screw-shared-data"
@@ -14,3 +19,8 @@ gdown --folder "https://drive.google.com/drive/folders/${FOLDER_ID}" -O "$OUT_DI
 
 echo "Done."
 echo "Saved to directory: $OUT_DIR"
+
+
+unzip screw-shared-data/screw-datasets.zip
+mv screw/test/ GLASS/datasets/mvtec/screw/
+mv screw/train/ GLASS/datasets/mvtec/screw/
